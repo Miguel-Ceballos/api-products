4a8f2a8df43cc1c21c11f7ec7530afea5da8cd82
Miguel Ceballos <mceballosleonides@gmail.com>
2024-05-14 15:08:26 -0600

    Create Model and Controller to Categorias

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Responses\ApiResponse;
+use App\Models\Category;
+use Exception;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        try {
+            $categories = Category::all();
+            return ApiResponse::success('Categories returned successfully.', 200, $categories);
+        } catch (Exception $exception) {
+            return ApiResponse::error('Error when returned categories: ' . $exception->getMessage(), $exception->getCode());
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Category $category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        //
+    }
+}
