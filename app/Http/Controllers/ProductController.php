6a60bbc268a0583c0af3cdecdb0eba04888ad641
Miguel Ceballos <mceballosleonides@gmail.com>
2024-05-16 10:01:23 -0600

    Create api functions

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Responses\ApiResponse;
 use App\Models\Product;
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\ValidationException;
 
 class ProductController extends Controller
 {
@@ -12,7 +17,12 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        try {
+            $products = Product::all();
+            return ApiResponse::success('Products successfully returned.', 200, $products);
+        } catch (Exception $exception) {
+            return ApiResponse::error('Error getting information.', 500);
+        }
     }
 
     /**
@@ -20,30 +30,74 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $request->validate([
+                'name' => [ 'required', 'string', 'max:50', Rule::unique('products') ],
+                'description' => [ 'required', 'max:255' ],
+                'price' => [ 'required', 'numeric', 'between:0,999999.99' ],
+                'stock' => [ 'required', 'integer' ],
+                'category_id' => [ 'required', 'exists:categories,id' ],
+                'brand_id' => [ 'required', 'exists:brands,id' ]
+            ]);
+            $product = Product::create($request->all());
+            return ApiResponse::success('Product created successfully', 201, $product);
+        } catch (ValidationException $exception) {
+            $errors = $exception->validator->errors()->toArray();
+            return ApiResponse::error('Validation error: ' . $errors, 422);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Product $products)
+    public function show($id)
     {
-        //
+        try {
+            $product = Product::findOrFail($id);
+            return ApiResponse::success('Product returned successfully.', 200, $product);
+        } catch (ModelNotFoundException $exception) {
+            return ApiResponse::error('Product not found.', 404);
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Product $products)
+    public function update(Request $request, $id)
     {
-        //
+        try {
+            $product = Product::findOrFail($id);
+            $request->validate([
+                'name' => [ 'required', 'string', 'max:50', Rule::unique('products')->ignore($product) ],
+                'description' => [ 'required', 'max:255' ],
+                'price' => [ 'required', 'numeric', 'between:0,999999.99' ],
+                'stock' => [ 'required', 'integer' ],
+                'category_id' => [ 'required', 'exists:categories,id' ],
+                'brand_id' => [ 'required', 'exists:brands,id' ]
+            ]);
+            $product->update($request->all());
+            return ApiResponse::success('Product updated successfully.', 200, $product);
+        } catch (ValidationException $exception) {
+            $errors = $exception->validator->errors()->toArray();
+            return ApiResponse::error('Validation error: ' . $errors, 422);
+        } catch (ModelNotFoundException $exception) {
+            return ApiResponse::error('Product not found.', 404);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Product $products)
+    public
+    function destroy($id)
     {
-        //
+        try {
+            $product = Product::findOrFail($id);
+            $product->delete();
+            return ApiResponse::success('Product deleted successfully', 200);
+        } catch (ModelNotFoundException $exception) {
+            return ApiResponse::error('Product not found.', 404);
+        }
+
     }
 }
