400cce4450ab0699753d2879995b42e189c21788
Miguel Ceballos <mceballosleonides@gmail.com>
2024-05-17 18:04:32 -0600

    Rename product purchase table to singular

---
+++ database/migrations/2024_05_17_204607_create_product_purchase_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up() : void
+    {
+        Schema::create('product_purchase', function(Blueprint $table) {
+            $table->id();
+            $table->foreignId('purchase_id');
+            $table->foreignId('product_id');
+            $table->decimal('price', 8, 2);
+            $table->integer('quantity');
+            $table->decimal('subtotal', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down() : void
+    {
+        Schema::dropIfExists('product_purchase');
+    }
+};
