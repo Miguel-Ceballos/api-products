6be7079a679ac3a2c6e636408528ca412f01262f
Miguel Ceballos <mceballosleonides@gmail.com>
2024-05-17 18:05:13 -0600

    Create Model to purchases and add routes

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,14 @@
 use App\Http\Controllers\BrandController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\PurchaseController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('categories', CategoryController::class);
 Route::apiResource('brands', BrandController::class);
 Route::apiResource('products', ProductController::class);
-Route::get('categories/{id}/products', [CategoryController::class, 'categoryProducts']);
-Route::get('brands/{id}/products', [BrandController::class, 'brandProducts']);
+Route::apiResource('products', ProductController::class);
+Route::apiResource('purchases', PurchaseController::class);
+
+Route::get('categories/{id}/products', [ CategoryController::class, 'categoryProducts' ]);
+Route::get('brands/{id}/products', [ BrandController::class, 'brandProducts' ]);
